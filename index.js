5862b42ec40e2c7cb62c1417704313ab2040c97e
Tom Xu <tomxu89@gmail.com>
2018-01-13 02:22:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 var recipes = {}
 
-function updateObjectWithKeyandValue(object, key, value) {
-  var newObject = Object.assign({}, {[key], value})
+function updateObjectWithKeyAndValue(object, key, value) {
+  var newObject = Object.assign({}, {[key]: value})
   return newObject
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value
+  return object
+}
+
+function deleteFromObjectByKey(object, key) {
+  var newObject = Object.assign({}, )
 }
