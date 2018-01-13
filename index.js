5d6a198e4a66b8aec8dd9efaed887fa668f557ef
Tom Xu <tomxu89@gmail.com>
2018-01-13 02:25:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var recipes = {}
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  var newObject = Object.assign({}, {[key]: value})
+  var newObject = Object.assign({}, object, {[key]: value})
   return newObject
 }
 
